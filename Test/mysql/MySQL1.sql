1d8aea907fb1824e9d06c110fc6d32e2721a9071
yiwang514 <1522354238@qq.com>
2026-07-11 23:36:54 +0800

    260711

--- Test/mysql/MySQL1.sql
+++ Test/mysql/MySQL1.sql
@@ -6,8 +6,17 @@ CREATE Table IF NOT EXISTS student(
     score DOUBLE(5,2) UNSIGNED NOT NULL COMMENT '成绩'
 )ENGINE=InnoDB CHARSET=UTF8 COMMENT='学生表';
 
-Alter TABLE student RENAME AS Stu;
+--需要用到 ALTER的情况（修改表结构）
+--任何改变表的“骨架”的操作都必须用 ALTER TABLE
 
+--DDL 分为三个基本命令：
+-- CREATE TABLE：创建表
+-- ALTER TABLE：修改表结构
+-- DROP TABLE：删除表
+
+Alter TABLE student RENAME AS Stu;
+--VARCHAR​ 表示“可变长度字符串”（Variable Character），用于存储文本数据。
+--括号里的 11​ 表示该字段允许存储的 最大字符数（不是字节数）。
 ALTER TABLE stu ADD phone VARCHAR(11) NOT NULL COMMENT '电话号码';
 DROP TABLE IF EXISTS student;
 
---
+++ note/SQL/MySQl增删改查.md
@@ -0,0 +1,12 @@
+1.
+--需要用到 ALTER的情况（修改表结构）
+--任何改变表的“骨架”的操作都必须用 ALTER TABLE
+
+--DDL 分为三个基本命令：
+-- CREATE TABLE：创建表
+-- ALTER TABLE：修改表结构
+-- DROP TABLE：删除表
+
+2.
+--VARCHAR​ 表示“可变长度字符串”（Variable Character），用于存储文本数据。
+--括号里的 11​ 表示该字段允许存储的 最大字符数（不是字节数）。 
